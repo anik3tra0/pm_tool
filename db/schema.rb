ea883787f17e6ad7852f8cb4825d310ebd4da544
anik3tra0 <anik3tra0@gmail.com>
2015-11-11 17:22:22 +0530

    Customer Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105142925) do
+ActiveRecord::Schema.define(version: 20151111115113) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20151105142925) do
     t.datetime "updated_at",   null: false
     t.string   "mobile"
     t.integer  "user_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "permissions", force: :cascade do |t|
